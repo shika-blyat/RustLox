fc44156b13a3a7b07f98b8dd1395615f2a1c1eba
shika-blyat <54486529+shika-blyat@users.noreply.github.com>
2019-10-27 01:15:51 +0200

    Fix some bugs

--- src/utility/tokenizer.rs
+++ src/utility/tokenizer.rs
@@ -1,135 +1,125 @@
-use std::error::Error;
-use std::fmt;
-
 #[derive(Debug)]
+#[allow(dead_code)]
+pub struct Token{
+    token_type: TokenType,
+    line: usize,
+    nbr_char: usize,
+}
+#[derive(Debug)]
+#[allow(dead_code)]
 pub enum TokenType {
     // Single-character tokens.
-    LeftParen{line: u16, nbr_char: u8},
-    RightParen{line: u16, nbr_char: u8},
-    LeftBrace{line: u16, nbr_char: u8},
-    RightBrace{line: u16, nbr_char: u8},
-    Comma{line: u16, nbr_char: u8},
-    Dot{line: u16, nbr_char: u8},
-    Minus{line: u16, nbr_char: u8},
-    Plus{line: u16, nbr_char: u8},
-    Semicolon{line: u16, nbr_char: u8},
-    Slash{line: u16, nbr_char: u8},
-    Star{line: u16, nbr_char: u8},
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
 
     // One or two character tokens.
-    Bang{line: u16, nbr_char: u8},
-    BangEqual{line: u16, nbr_char: u8},
-    Equal{line: u16, nbr_char: u8},
-    EqualEqual{line: u16, nbr_char: u8},
-    Greater{line: u16, nbr_char: u8},
-    GreaterEqual{line: u16, nbr_char: u8},
-    Less{line: u16, nbr_char: u8},
-    LessEqual{line: u16, nbr_char: u8},
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
 
     // Literals.
-    Identifier{line: u16, nbr_char: u8},
-    String{line: u16, nbr_char: u8},
-    Number{line: u16, nbr_char: u8},
+    Identifier,
+    String,
+    Number,
 
     // Keywords.
-    And{line: u16, nbr_char: u8},
-    Class{line: u16, nbr_char: u8},
-    Else{line: u16, nbr_char: u8},
-    False{line: u16, nbr_char: u8},
-    Fun{line: u16, nbr_char: u8},
-    For{line: u16, nbr_char: u8},
-    If{line: u16, nbr_char: u8},
-    Nil{line: u16, nbr_char: u8},
-    Or{line: u16, nbr_char: u8},
-    Print{line: u16, nbr_char: u8},
-    Return{line: u16, nbr_char: u8},
-    Super{line: u16, nbr_char: u8},
-    This{line: u16, nbr_char: u8},
-    True{line: u16, nbr_char: u8},
-    Var{line: u16, nbr_char: u8},
-    While{line: u16, nbr_char: u8},
-    Eof{line: u16, nbr_char: u8},
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+    Eof,
+
+    //Error
+    InvalidToken,
 }
 
-#[derive(Debug)]
-pub struct InvalidToken{
-    line: u16,
-    nbr_char: u8,
-    token: char,
+
+pub struct TokenList{
+    pub program : String,
+    pub token_list: Vec<Token>,
+    line: usize,
+    start:usize,
+    current: usize,
+    nbr_char: usize,
 }
 
-impl fmt::Display for InvalidToken {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        writeln!(f, "Invalid token")
+impl TokenList{
+    pub fn new(program: &str) -> TokenList{
+        TokenList{program: program.to_string(), token_list: vec![],line: 1, nbr_char:1, current:0, start:0}
     }
-}
-
-impl Error for InvalidToken {}
-
-pub fn tokenize(program: &str)-> Result<Vec<TokenType>, Vec<InvalidToken>>  {
-
-    let mut line = 1;
-    let mut nbr_char = 0;
-    let mut token_list = vec![];
-    let mut errors = vec![];
-    let mut lookahead = false;
-    let mut looked_char = '0'; // Don't blame me for this name :(
-
-    fn look_to(looked: char){
-        |mut looked_char| looked_char = looked;
-        |mut lookahead| lookahead = true;
+    pub fn tokenize(&mut self) -> &Vec<Token>{
+        let mut last_char = '\0';
+        let mut lookahead = false;
+        let mut looked_char = '\0';
+        let mut looked_token: TokenType = TokenType::Eof;
+        for (_k,i) in self.program.chars().enumerate(){
+            if lookahead{
+                if i == looked_char{
+                    lookahead = false;
+                    self.token_list.push(Token{token_type: TokenType::LeftParen, line: self.line, nbr_char: self.nbr_char})
+                }
+                continue;
+            }
+            match i{
+                '(' => self.token_list.push(Token{token_type: TokenType::LeftParen, line: self.line, nbr_char: self.nbr_char}),
+                ')' => self.token_list.push(Token{token_type: TokenType::RightParen, line: self.line, nbr_char: self.nbr_char}),
+                '{' => self.token_list.push(Token{token_type: TokenType::LeftBrace, line: self.line, nbr_char: self.nbr_char}),
+                '}' => self.token_list.push(Token{token_type: TokenType::RightBrace, line: self.line, nbr_char: self.nbr_char}),
+                ',' => self.token_list.push(Token{token_type: TokenType::Comma, line: self.line, nbr_char: self.nbr_char}),
+                ';' => self.token_list.push(Token{token_type: TokenType::Semicolon, line: self.line, nbr_char: self.nbr_char}),
+                '.' => self.token_list.push(Token{token_type: TokenType::Dot, line: self.line, nbr_char: self.nbr_char}),
+                '+' => self.token_list.push(Token{token_type: TokenType::Plus, line: self.line, nbr_char: self.nbr_char}),
+                '-' => self.token_list.push(Token{token_type: TokenType::Minus, line: self.line, nbr_char: self.nbr_char}),
+                '*' => self.token_list.push(Token{token_type: TokenType::Star, line: self.line, nbr_char: self.nbr_char}),
+                '=' => if last_char == '=' {
+                            self.token_list.pop();
+                            self.token_list.push(Token{token_type: TokenType::EqualEqual, line: self.line, nbr_char: self.nbr_char})
+                        } else if last_char == '<' {
+                            self.token_list.pop();
+                            self.token_list.push(Token{token_type: TokenType::LessEqual, line: self.line, nbr_char: self.nbr_char})
+                        } else if last_char == '>' {
+                            self.token_list.pop();
+                            self.token_list.push(Token{token_type: TokenType::GreaterEqual, line: self.line, nbr_char: self.nbr_char})
+                        } else if last_char == '!' {
+                            self.token_list.pop();
+                            self.token_list.push(Token{token_type: TokenType::BangEqual, line: self.line, nbr_char: self.nbr_char})
+                        } else {
+                            self.token_list.push(Token{token_type: TokenType::Equal, line: self.line, nbr_char: self.nbr_char})
+                        },
+                '<' => self.token_list.push(Token{token_type: TokenType::Less, line: self.line, nbr_char: self.nbr_char}),
+                '>' => self.token_list.push(Token{token_type: TokenType::Greater, line: self.line, nbr_char: self.nbr_char}),
+                '!' => self.token_list.push(Token{token_type: TokenType::Bang, line: self.line, nbr_char: self.nbr_char}),
+                _ => self.token_list.push(Token{token_type: TokenType::InvalidToken, line: self.line, nbr_char: self.nbr_char}),
+            }
+            last_char = i;
+        }
+        self.token_list.push(Token{token_type: TokenType::Eof, line:self.program.len(), nbr_char:self.nbr_char});
+        &self.token_list
     }
-
-    for (k,i) in program.chars().enumerate(){
-        nbr_char+=1;
-        if let '\n' = i{
-            line += 1 ;
-            nbr_char = 0;
-            lookahead = false;
-        }
-        if lookahead && i == looked_char {
-            lookahead = false;
-        }
-        if !lookahead{
-        match i{
-            '(' => token_list.push(TokenType::LeftParen{line, nbr_char}),
-            ')' => token_list.push(TokenType::RightParen{line, nbr_char}),
-            '{' => token_list.push(TokenType::LeftBrace{line, nbr_char}),
-            '}' => token_list.push(TokenType::RightBrace{line, nbr_char}),
-            ',' => token_list.push(TokenType::Comma{line, nbr_char}),
-            ';' => token_list.push(TokenType::Semicolon{line, nbr_char}),
-            '.' => token_list.push(TokenType::Dot{line, nbr_char}),
-            '+' => token_list.push(TokenType::Plus{line, nbr_char}),
-            '-' => token_list.push(TokenType::Minus{line, nbr_char}),
-            '*' => token_list.push(TokenType::Star{line, nbr_char}),
-            '=' => match program.chars().nth(k+1) {
-                    Some('=') => token_list.push(TokenType::EqualEqual{line, nbr_char}),
-                    _ => token_list.push(TokenType::Equal{line, nbr_char}),
-                },
-            '>' => match program.chars().nth(k+1) {
-                    Some('=') => token_list.push(TokenType::GreaterEqual{line, nbr_char}),
-                    _ => token_list.push(TokenType::Greater{line, nbr_char}),
-                },
-            '<' => match program.chars().nth(k+1) {
-                    Some('=') => token_list.push(TokenType::LessEqual{line, nbr_char}),
-                    _ => token_list.push(TokenType::Less{line, nbr_char}),
-                },
-            '!' => match program.chars().nth(k+1) {
-                    Some('=') => token_list.push(TokenType::BangEqual{line, nbr_char}),
-                    _ => token_list.push(TokenType::Bang{line, nbr_char})
-                },
-            '/' => match program.chars().nth(k+1) {
-                    Some('/') => look_to('/'),
-                    _ => token_list.push(TokenType::Slash{line, nbr_char})
-                },
-            ' ' => (),
-            '\n' => (),
-            '\r' => (),
-            '\t' => (),
-            _   => errors.push(InvalidToken{line, nbr_char, token: i}),
-        };
-        }
-    }
-    token_list.push(TokenType::Eof{line, nbr_char: program.len() as u8});
-    if errors.len() == 0 {Ok(token_list)} else {Err(errors)}
 }
