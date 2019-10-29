2527ebb4fd58c5dd30a89a0a133c3988ac6128a7
abdel <abdelzighem1@gmail.com>
2019-10-29 03:55:19 +0100

    Refactor everything from scratch

--- target/debug/deps/lox-45a0ac5c841a6653.d
+++
@@ -1,8 +0,0 @@
-C:\Users\abdel\Documents\Rust\RustLox\target\debug\deps\lox-45a0ac5c841a6653.exe: src\main.rs src\utility\mod.rs src\utility\tokenizer.rs src\utility\tokens.rs
-
-C:\Users\abdel\Documents\Rust\RustLox\target\debug\deps\lox-45a0ac5c841a6653.d: src\main.rs src\utility\mod.rs src\utility\tokenizer.rs src\utility\tokens.rs
-
-src\main.rs:
-src\utility\mod.rs:
-src\utility\tokenizer.rs:
-src\utility\tokens.rs:
--- target/debug/deps/lox-fd10f00bb79aabbc.d
+++
@@ -1,8 +0,0 @@
-C:\Users\abdel\Documents\Rust\RustLox\target\debug\deps\lox-fd10f00bb79aabbc.rmeta: src\main.rs src\utility\mod.rs src\utility\tokenizer.rs src\utility\tokens.rs
-
-C:\Users\abdel\Documents\Rust\RustLox\target\debug\deps\lox-fd10f00bb79aabbc.d: src\main.rs src\utility\mod.rs src\utility\tokenizer.rs src\utility\tokens.rs
-
-src\main.rs:
-src\utility\mod.rs:
-src\utility\tokenizer.rs:
-src\utility\tokens.rs:
