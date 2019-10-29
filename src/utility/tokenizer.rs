5d18a9bc6745705189b4a0ce6d108ff37bf607cc
abdel <abdelzighem1@gmail.com>
2019-10-29 06:46:00 +0100

    add numbers lexing

--- src/utility/tokenizer.rs
+++ src/utility/tokenizer.rs
@@ -52,7 +52,10 @@ impl Scanner {
     }
     fn scan_token(&mut self) {
         let c = self.advance();
-        println!("{}", c);
+        if c.is_digit(10){
+            self.get_number();
+            return
+        }
         match c {
             '(' => self.add_token(TokenType::LeftParen),
             ')' => self.add_token(TokenType::RightParen),
@@ -108,8 +111,20 @@ impl Scanner {
             _ => self.add_token(TokenType::InvalidToken),
         }
     }
+    fn get_number(&mut self){
+        while self.peek().is_digit(10){
+            self.advance();
+        }
+        if self.peek() == '.' && self.peek_next().is_digit(10){
+            self.advance();
+            while self.peek().is_digit(10){
+                self.advance();
+            } 
+        }
+        let number = self.source[self.start..self.current].parse::<f64>().unwrap();
+        self.add_token(TokenType::Number(number));
+    }
     fn get_string(&mut self){
-        println!("a");
         while self.peek() != '"' && self.is_full(){
             if self.peek() == '\n' {
                 self.line+=1;
@@ -123,7 +138,6 @@ impl Scanner {
         self.advance(); // The closing '"'
         let value = self.source[self.start+1..self.current-1].to_string();
         self.add_token(TokenType::String(value));
-        println!("c");
     }
     fn match_next_char(&mut self, c: char) -> bool {
         if !self.is_full() {
@@ -139,6 +153,12 @@ impl Scanner {
             return '\0'
         }
         self.source.chars().nth(self.current).unwrap()
+    }
+    fn peek_next(&self) -> char{
+        if self.current+1 >= self.source.len(){
+            return '\0'
+        }
+        return self.source.chars().nth(self.current+1).unwrap();
     }
     fn add_token(&mut self, token_type: TokenType) {
         let text = self.source[self.start..self.current].to_string();
