81f58c70ade20f19c94394acbb3b09c13ef4ab47
abdel <abdelzighem1@gmail.com>
2019-10-29 03:55:39 +0100

    Refactor everything from scratch

---
+++ old_files/mainv1.rs
@@ -0,0 +1,53 @@
+/*
+use std::env::args;
+use std::fs::File;
+use std::io::stdin;
+use std::io::stdout;
+use std::io::Error;
+use std::io::Read;
+use std::io::Write;
+use utility::tokens::TokenType;
+fn main() {
+    let token = TokenType::Dot;
+    println!("{}",token.to_string());
+}
+fn run_interpreter() {
+    if let Some(path) = args().nth(1) {
+        let file = open_file(&path).unwrap();
+        execute_file(&file.trim());
+    } else {
+        loop {
+            let mut line = String::new();
+            print!(">>> ");
+            if let Err(a) = stdout().flush() {
+                eprintln!("An error occured while reading the line:\n{}", a);
+            }
+            stdin().read_line(&mut line).unwrap();
+            match execute_line(&line.trim()) {
+                true => (),
+                false => break,
+            };
+        }
+    }
+}
+fn execute_line(line: &str) -> bool {
+    if line.to_lowercase() == "quit()" {
+        return false;
+    } 
+    if line.chars().nth(line.len()).unwrap().is_digit(10){
+        let line = &(line.to_string()+ " ");
+        let mut program = TokenList::new(line);
+        println!("{:?}", program.tokenize());
+    } else {
+        let mut program = TokenList::new(&(line.to_string()+ " "));
+        println!("{:?}", program.tokenize());
+    }
+    true
+}
+fn execute_file(_file: &str) {}
+fn open_file(path: &str) -> Result<String, Error> {
+    let mut file = File::open(path)?;
+    let mut program = String::new();
+    file.read_to_string(&mut program)?;
+    Ok(program)
+}*/
